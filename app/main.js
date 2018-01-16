c7948b4765a04add2c191502b6dc868c06212aee
[ianizaguirre] <ian@izaguir.re>
2018-01-15 22:58:57 -0500

    control second of content flash by waiting till app is ready to show

--- app/main.js
+++ app/main.js
@@ -6,7 +6,16 @@
 const { app, BrowserWindow } = require('electron');
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    width: 300,
+    height: 600,
+    show: false
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
